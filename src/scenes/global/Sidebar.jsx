3e855defd928a3ea7219e83f23a409683a711ba0
Khrystyna <khrystyna.tyvoniuk@gmail.com>
2023-04-25 17:50:50 +0200

    css styling

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -29,6 +29,18 @@ const Sidebar = () => {
         "& .pro-sidebar-inner": {
           background: `${colors.primary[400]} !important`,
         },
+        "& .pro-icon-wrapper": {
+          background: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
       }}
     ></Box>
   );
